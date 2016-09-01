a594633acaddbdcdce3b794a6fae72fb7760d6f7
eyalcohen4 <Eyalcohen4.ec@gmail.com>
2016-09-01 10:57:45 +0300

    Moving to SP

--- js/init.js
+++ js/init.js
@@ -3,5 +3,15 @@
     $('.button-collapse').sideNav();
 
 
+    $('.aToProjects').on("click", function() {
+        $('#skills').hide();
+        $('#contact').hide();
+        $('#projects').show();
+    });
+
+    $('.aToSkills').on("click", function() {
+        $('#projects').hide();
+        $('#skills').show();
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
